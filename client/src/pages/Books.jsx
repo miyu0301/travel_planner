4de2480dec879fc7a11623f08112b38486a4e627
Miyu <75245305+miyu0301@users.noreply.github.com>
2023-07-03 20:22:55 -0700

    added coding for books

--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const Books = () => {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        const fechAllBooks = async () => {
+            try {
+                const res = await axios.get("http://localhost:3000/books")
+                console.log(res)
+            }catch(err){
+                console.log(err)
+            }
+        }
+        fechAllBooks()
+    }, [])
     return (
         <div>Books</div>
     )
