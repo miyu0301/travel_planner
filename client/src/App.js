5e2ba0881dba0ded029088f6e9c12774915d55bd
Miyu <75245305+miyu0301@users.noreply.github.com>
2023-07-03 20:15:22 -0700

    added router and axios packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  BrowserRouter,
+  Routes,
+} from "react-router-dom";
+import Add from "./pages/Add";
+import Books from "./pages/Books";
+import Update from "./pages/Update";
 
 function App() {
   return (
     <div className="App">
-      hello
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Books/>}/>
+          <Route path="/add" element={<Add/>}/>
+          <Route path="/update" element={<Update/>}/>
+        </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
